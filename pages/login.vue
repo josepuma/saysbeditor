1cb9d294238e98753909b023433011af5ab4b5af
Agustín <agustingonzalezmurua@gmail.com>
2019-08-05 16:56:31 -0400

    Added BG SVG for login

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
     .uk-grid(uk-height-viewport="expand")
-        .hero.uk-width-2-3
-            // TODO: Add pretty svg
-            | Pretty SVG Here
+        .side.uk-width-2-3
+            img.logo(src="@/assets/Background.svg" uk-svg)
         .login.uk-width-1-3.uk-padding.uk-flex.uk-flex-column.uk-flex-center
-            .uk-section
+            .uk-section.headers
                 h1.uk-margin-small Better Name Pending
                 h2.uk-margin-remove Create, Innovate & Share
             .uk-section.uk-flex.uk-flex-column
@@ -12,7 +11,8 @@
                     a.uk-link sign up
                 input.secondary(type="text" placeholder="Username")
                 input.uk-margin-top.secondary(type="text" placeholder="Password")
-                button.uk-button.uk-button-secondary.uk-width-1-4.uk-margin-small-top Log in
+                div.uk-width-1-1.uk-margin-small-top
+                    button.uk-button.uk-button-secondary.uk-width-1-4.uk-align-right.uk-margin-remove Log in
                 span.uk-margin-small-top.uk-text-right.uk-text-small Forgot password?
 
 </template>
@@ -27,7 +27,23 @@ export default {
 @import "@/assets/styles/_variables.scss";
 
 .login {
+    z-index: 1;
     background-color: $background-alt;
+}
+
+.headers {
+    > * {
+        font-weight: bold;
+    }
+}
+
+.side {
+    overflow: hidden;
+    max-height: 100vh;
+    .logo {
+        min-width: 100%;
+        transform: scale(1.5);
+    }
 }
 </style>
 
