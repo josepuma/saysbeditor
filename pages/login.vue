6e1a83f37329de59773157bc5fbacef387b5bc3a
Agustín <agustingonzalezmurua@gmail.com>
2019-08-05 22:38:18 -0400

    Added form into login

--- pages/login.vue
+++ pages/login.vue
@@ -6,11 +6,11 @@
             .uk-section.headers
                 h1.uk-margin-small Better Name Pending
                 h2.uk-margin-remove Create, Innovate & Share
-            .uk-section.uk-flex.uk-flex-column
+            form.uk-section.uk-flex.uk-flex-column
                 span.uk-margin-small-bottom.uk-text-right Are you new around here? 
                     a.uk-link sign up
-                input.secondary(type="text" placeholder="Username")
-                input.uk-margin-top.secondary(type="text" placeholder="Password")
+                input.secondary(type="email" placeholder="Username" required)
+                input.uk-margin-top.secondary(type="password" placeholder="Password" required)
                 div.uk-width-1-1.uk-margin-small-top
                     button.uk-button.uk-button-secondary.uk-width-1-4.uk-align-right.uk-margin-remove Log in
                 span.uk-margin-small-top.uk-text-right.uk-text-small Forgot password?
