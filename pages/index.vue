d6ede9c2afb91e5ae84d98483cba190635a08cf0
Agustín <agustingonzalezmurua@gmail.com>
2019-08-19 23:45:21 -0400

    Basic layouting

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,16 @@
 <template lang="pug">
-
-    .uk-section
-        .uk-container
-            div(class="uk-child-width-1-4@m", uk-grid)
-                div
-                    h2 Hola
-                div
-                    h2 Mundo
-                div
-                    h2 Nice
-                div
-                    h2 Try
-
+    div(class="uk-grid uk-grid-collapse uk-padding-large" uk-height-viewport)
+        div(class="uk-container uk-width-2-3@l uk-grid")
+            div(class="")
+                h1 Welcome Back {{ this.$auth.user.username }}!
+                h2.uk-margin-remove Your projects
+                
+        div(class="uk-container uk-width-1-3@l uk-padding-remove")
+            .uk-card.uk-height-1-1
+                .uk-card-header
+                    h3.uk-card-title Recent Activity
+                .uk-card-body
+                    p Nice job 
 </template>
 
 <script>
