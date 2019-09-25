1849e91d4169223cca2056d95de5389348abbd30
Agustín <agustingonzalezmurua@gmail.com>
2019-09-25 13:34:05 -0300

    Reincorporated margin to separate login button from header

--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,7 @@
     div(class="login uk-width-1-3@l u@k-width-1-1@s uk-padding")
       div(class="uk-flex uk-flex-column uk-flex-center uk-flex-middle uk-width-1-1 uk-height-1-1")
         h1.uk-margin-small Better Name Pending
-        h2.uk-margin-remove Create, Innovate & Share
+        h2 Create, Innovate & Share
         a(href="/api/connect/discord").uk-width-1-1
           button(type="button").uk-button.uk-button-primary.uk-width-1-1 Log in with Discord!
 </template>
