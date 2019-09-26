182c9c4b38e1e6fe8a5df4c69a604dfd1390c89c
Agustín <agustingonzalezmurua@gmail.com>
2019-09-26 13:48:41 -0300

    Added some comments in Discord callback

--- pages/connect/discord.vue
+++ pages/connect/discord.vue
@@ -8,12 +8,16 @@ const Cookie = process.client ? require('js-cookie') : undefined
 
 export default {
   async created() {
+    // Extract all query parameter strings
     const search = this.$route.fullPath.replace('/connect/discord?', '')
-    const response = await this.$axios.get(`/api/auth/discord/callback?${search}`) // Let the API retrieve the token
+    // Let the API retrieve the token for us and receive the auth JWT
+    const response = await this.$axios.get(`/api/auth/discord/callback?${search}`)
     const auth = `Bearer ${response.data.jwt}`
     const user = response.data.user
 
     await this.$store.dispatch('setAuth', { auth, user })
+
+    // Set cookies for further visits / reload
     Cookie.set('auth', auth, { expires: 30 })
     Cookie.set('user', user, { expires: 30 })
 
