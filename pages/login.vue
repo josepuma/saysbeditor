123d5c56afa1b70056e1ed2b9d11f532d584a6f2
Agustín <agustingonzalezmurua@gmail.com>
2019-09-27 17:03:24 -0300

    Updated login styles

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
-  div(class=" uk-grid uk-grid-collapse" uk-height-viewport)
-    div(class="side uk-width-2-3@l uk-width-1-1@s uk-flex" uk-height-match=".login")
-    div(class="login uk-width-1-3@l u@k-width-1-1@s uk-padding")
+  div(class="uk-grid uk-grid-collapse" uk-height-viewport)
+    div(class="uk-width-2-3@l uk-width-1-1@s uk-flex").side
+    div(class="uk-width-1-3@l u@k-width-1-1@s uk-padding").login
       div(class="uk-flex uk-flex-column uk-flex-center uk-flex-middle uk-width-1-1 uk-height-1-1")
         h1.uk-margin-small Better Name Pending
         h2 Create, Innovate & Share
-        a(href="/api/connect/discord").uk-width-1-1
-          button(type="button").uk-button.uk-button-primary.uk-width-1-1 Log in with Discord!
+        a(
+          href="/api/connect/discord"
+          class="uk-button uk-button-primary"
+          class="uk-width-1-1"
+        ) Log in with Discord!
 </template>
 
 <script>
