6293e8cda8defd5b6847c015d0d4f20e1be2c6a0
Agustín <agustingonzalezmurua@gmail.com>
2019-09-29 02:34:07 -0300

    New Project view WIP

--- pages/@me/projects/new.vue
+++ pages/@me/projects/new.vue
@@ -1,10 +1,52 @@
 <template lang="pug">
-  div New Project
+  div
+    h1 Create a new project
+    form(uk-grid).uk-grid-small
+
+      .uk-width-1-1
+        label(for="form-project-name").uk-form-label Name
+        .uk-form-controls
+          input(
+            name="form-project-name"
+            type="text"
+            placeholder="Something easy to remember"
+            v-model="project.name"
+          ).uk-input
+
+      div(class="uk-width-1-2@m uk-width-1-1@s")
+        label(for="form-project-song-title").uk-form-label Song title
+          .uk-form-controls
+            input(
+              name="form-project-song-title"
+              type="text"
+              placeholder="Ra ri ru re ro"
+              v-model="project.title"
+            ).uk-input
+
+      div(class="uk-width-1-2@m uk-width-1-1@s")
+        label(for="form-project-name").uk-form-label Song artist
+          .uk-form-controls
+            input(
+              name="form-project-song-artist"
+              type="text"
+              placeholder="John Doe"
+              v-model="project.artist"
+            ).uk-input
 </template>
 
 <script>
 export default {
   middleware: 'authenticated',
-  layout: 'authenticated'
+  layout: 'authenticated',
+  data() {
+    return {
+      project: {
+        name: '',
+        artist: '',
+        title: '',
+        tags: []
+      }
+    }
+  }
 }
 </script>
